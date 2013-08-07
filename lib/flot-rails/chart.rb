5c30b8849e357c77fde972fa7ff776c7566214ed
Holger Arndt <arndt@comcard.de>
2013-08-07 07:51:22 +0200

    added width & height option for the container div

--- lib/flot-rails/chart.rb
+++ lib/flot-rails/chart.rb
@@ -1,40 +1,40 @@
 module Flot
   class InvalidDataset < RuntimeError; end
-    
+
   BaseOpts = {
     :bar => {bars: {show: true}, prefix: :bar},
     :line => {lines: {show: true}, prefix: :line},
     :point => {points: {show: true}, prefix: :point},
     :pie => {series: {pie: {show: true}}, prefix: :pie}
   };
-  
+
   def on_click
   end
-  
+
   def on_hover
   end
-  
+
   def chart(dataset, opts={})
     @__chart_script_content ||= []
-    
+
     split_flag = opts.delete(:split)
     uniq_name = [opts.delete(:prefix), :chart, dataset.join.hash.abs.to_s].compact.join('_')
-    
+
     # dataset will be some kind of tripple nested arrays or an array of hashes with an 2-dimensional-array data-element
     raise InvalidDataset, dataset.inspect unless dataset.kind_of?(Array)
     dataset.each do |ele|
       raise InvalidDataset unless ele.kind_of?(Array) or (ele.kind_of?(Hash) and ele.has_key?(:data))
     end
-    
-    div = "<div class=\"inner\" id=\"#{uniq_name}\" style=\"width:600px;height:300px;\"></div>"
+
+    div = "<div class=\"inner\" id=\"#{uniq_name}\" style=\"width:#{opts.delete(:width) || '600px'};height:#{opts.delete(:height) || '300px'};\"></div>"
     script = <<-HTML
 <script type='text/javascript'>
   $(window).load(function () {
-    $.plot($("##{uniq_name}"), #{dataset.to_s.gsub(/:(\w*)=>/, '\1: ').gsub(/(\[|\{)(\[|\{)/, '\1' + "\n" + '\2  ').gsub(/],/, "],\n")}#{(', ' + opts.to_s.gsub(/:(\w*)=>/, '\1: ')) unless opts.empty? } ); 
+    $.plot($("##{uniq_name}"), #{dataset.to_s.gsub(/:(\w*)=>/, '\1: ').gsub(/(\[|\{)(\[|\{)/, '\1' + "\n" + '\2  ').gsub(/],/, "],\n")}#{(', ' + opts.to_s.gsub(/:(\w*)=>/, '\1: ')) unless opts.empty? } );
   });
 </script>
     HTML
-    
+
     if split_flag
       @__chart_script_content << script
       return raw div
@@ -42,17 +42,17 @@ module Flot
       return raw div + script
     end
   end
-  
+
   def yield_chart_script_at(yield_tag)
     content_for yield_tag do raw @__chart_script_content.join end
   end
-    
+
   def self.chart_type(name)
     define_method [name, 'chart'].join('_') do |ds, opts={}|
       chart(ds, BaseOpts[name].merge(opts))
     end
   end
-  
+
   chart_type :bar
   chart_type :line
   chart_type :point
@@ -81,18 +81,18 @@ $("#placeholder").bind("plothover", function (event, pos, item) {
     if (item) {
         if (previousPoint != item.dataIndex) {
             previousPoint = item.dataIndex;
-            
+
             $("#tooltip").remove();
             var x = item.datapoint[0].toFixed(2),
                 y = item.datapoint[1].toFixed(2);
-            
+
             showTooltip(item.pageX, item.pageY,
                         item.series.label + " of " + x + " = " + y);
         }
     }
     else {
         $("#tooltip").remove();
-        previousPoint = null;            
+        previousPoint = null;
     }
 });
 
