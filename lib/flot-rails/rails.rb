905ef2c6503d39a115acea989ff761b04f8f535c
kia84 <kia84@mail.msiu.ru>
2014-02-26 12:13:10 +0400

    Magic encoding! :sparkles:

--- lib/flot-rails/rails.rb
+++ lib/flot-rails/rails.rb
@@ -1,2 +1,4 @@
+# -*- encoding : utf-8 -*-
+
 require 'flot-rails/rails/engine'
 require 'flot-rails/rails/version'
--- lib/flot-rails/rails/engine.rb
+++ lib/flot-rails/rails/engine.rb
@@ -1,3 +1,5 @@
+# -*- encoding : utf-8 -*-
+
 module Flot
   module Rails
     class Engine < ::Rails::Engine
